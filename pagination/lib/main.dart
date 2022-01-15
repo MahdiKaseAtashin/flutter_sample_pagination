d66b6d2e1a416efdb8361c31966e9f3bbd74c95a
mahdi <MahdiKaseAtashin@gmail.com>
2022-01-15 11:31:25 +0330

    implement a simple pagination

--- pagination/lib/main.dart
+++ pagination/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,6 +20,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ScrollController _scrollController = ScrollController();
   List<String> items = [];
   bool loading = false, allLoaded = false;
 
@@ -33,7 +32,7 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(const Duration(milliseconds: 500));
     List<String> newData = items.length >= 60
         ? []
-        : List.generate(20, (index) => "List Item ${index + items.length}");
+        : List.generate(20, (index) => "List Item ${index + items.length + 1}");
     if (newData.isNotEmpty) {
       items.addAll(newData);
     }
@@ -41,6 +40,26 @@ class _HomePageState extends State<HomePage> {
       loading = false;
       allLoaded = newData.isEmpty;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mockFetch();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+              _scrollController.position.maxScrollExtent &&
+          !loading) {
+        print('new data call');
+        mockFetch();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -52,11 +71,47 @@ class _HomePageState extends State<HomePage> {
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
-          return Container(
-            child: const Center(
+          if (items.isNotEmpty) {
+            return Stack(children: [
+              ListView.separated(
+                  controller: _scrollController,
+                  itemBuilder: (context, index) {
+                    if (index < items.length) {
+                      return ListTile(
+                        title: Text(items[index]),
+                      );
+                    } else {
+                      return SizedBox(
+                        width: constraints.maxWidth,
+                        height: 80,
+                        child: const Center(child: Text('all loaded!')),
+                      );
+                    }
+                  },
+                  separatorBuilder: (context, index) {
+                    return const Divider(
+                      height: 1,
+                    );
+                  },
+                  itemCount: items.length + (allLoaded ? 1 : 0)),
+              if (loading) ...{
+                Positioned(
+                    left: 0,
+                    bottom: 0,
+                    child: SizedBox(
+                      height: 80,
+                      width: constraints.maxWidth,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ))
+              }
+            ]);
+          } else {
+            return const Center(
               child: CircularProgressIndicator(),
-            ),
-          );
+            );
+          }
         },
       ),
     );
